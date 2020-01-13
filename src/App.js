912b191df7d91cc0f6aafd814dae432246eefba7
glassrod <asafo@consci.ai>
2020-01-13 22:28:11 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was Changed by Oron</p>
+        <p>I was Changed by Oron !!!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
